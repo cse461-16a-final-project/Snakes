1421c06d62f1a95851b1a7b4e56fe2e208a943fa
Jianhao Zhou <jianhaoz@cs.washington.edu>
2016-12-08 21:57:28 -0800

    Server Demo done

--- server/server.py
+++ server/server.py
@@ -2,16 +2,17 @@ import socketio, eventlet, thread, threading, time
 from flask import Flask, render_template
 from json import JSONEncoder
 from collections import deque
+from game import Board, Snake
 
 class Server:
 	def __init__(self):
 		self.encoder = JSONEncoder()
 		self.sio = socketio.Server(async_mode='eventlet')
 		self.app = Flask(__name__)
-		self.x = 200
-		self.y = 200
-		self.dir = 37
-		self.controlBuffer = deque([])
+
+		# self.users = {}
+
+		self.board = Board()
 		self.thread = None
 
 		@self.app.route('/')
@@ -26,22 +27,28 @@ class Server:
 		def connect(sid, environ):
 			print 'connect ', sid
 
-		@self.sio.on('start')
-		def start(sid, data):
-			pass
+		@self.sio.on('new_user')
+		def new_user(sid, data):
+			# user = User()
+			snake = Snake(self.board, sid, data)
+			# user.snake = snake
+			# self.users[sid] = user
+			self.board.addSnake(snake)
+			self.sio.emit('accept', str(sid))
 
 		@self.sio.on('key')
 		def key(sid, data):
 			keyCode = int(data)
-			if keyCode >= 37 and keyCode <= 40:
-				self.controlBuffer.append(keyCode)
+			if keyCode >= 37 and keyCode <= 40 and sid in self.board.snakes:
+				self.board.snakes[sid].controlBuffer.append(keyCode - 37)
 
 		@self.sio.on('disconnect')
 		def disconnect(sid):
 			print 'disconnect ', sid
 
 	def update(self):
-		self.sio.emit('game state', self.encoder.encode((self.x, self.y)))
+		self.sio.emit('game_state', self.encoder.encode(self.board.toState()))
+		print self.encoder.encode(self.board.toState())
 
 	def tick(self):
 		# pass
@@ -50,19 +57,13 @@ class Server:
 		self.sio.start_background_task(self.tick)
 
 	def runGame(self):
-		if self.controlBuffer:
-			self.dir = self.controlBuffer.popleft()
-		direction = self.dir
-		if direction == 37:
-			self.x -= 20
-		elif direction == 38:
-			self.y -= 20
-		elif direction == 39:
-			self.x += 20
-		elif direction == 40:
-			self.y += 20
-		# self.update()
-		self.sio.sleep(0.5)
+		for snake in self.board.snakes.values():
+			# if snake.controlBuffer:
+				# user.lastDirection = user.controlBuffer.popleft()
+			# direction = user.lastDirection
+			snake.move()
+				# die
+		self.sio.sleep(0.2)
 		self.sio.start_background_task(self.runGame)
 
 	def start(self):
@@ -71,6 +72,11 @@ class Server:
 
 		# deploy as an eventlet WSGI server
 		eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8000)), self.app, log_output=False)
+# class User:
+# 	def __init__(self):
+# 		self.controlBuffer = deque()
+# 		self.lastDirection = 0
+# 		self.snake = None
 
 if __name__ == '__main__':
 	server = Server()
